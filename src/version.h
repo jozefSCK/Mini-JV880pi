a88f473b18e0b9fc33930c27b53613ebe0401d19
Gene J.B. <sterr@list.ru>
2025-11-06 21:12:04 +0000

    History

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION_STRING "v1.0.6-10-gd617c39-dirty"
-#define VERSION_SHORT "v1.0.6"  
+#define VERSION_STRING "v1.1.0-0-ge74703d"
+#define VERSION_SHORT "v1.1.0"  
 #define VERSION_IS_DIRTY "" == "dirty" || echo "clean"
 
 #endif // VERSION_H
